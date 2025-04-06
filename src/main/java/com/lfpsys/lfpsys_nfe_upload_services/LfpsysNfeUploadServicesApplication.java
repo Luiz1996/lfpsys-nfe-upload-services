d54c1488905adb79d6a055eb6d1ce37d96379359
Luiz1996 <luiz_flavio_p@hotmail.com>
2025-04-06 13:02:34 -0300

    Initial commit.

---
+++ src/main/java/com/lfpsys/lfpsys_nfe_upload_services/LfpsysNfeUploadServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lfpsys.lfpsys_nfe_upload_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfpsysNfeUploadServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfpsysNfeUploadServicesApplication.class, args);
+	}
+
+}
